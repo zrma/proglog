1cf58c45b4c88a662de03786e702a03ae3b6ee38
zrma <bulbitain@gmail.com>
2024-06-23 19:33:10 +0900

    add log

---
+++ pkg/log/log.go
@@ -0,0 +1,206 @@
+package log
+
+import (
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/zrma/proglog/pkg/pb"
+)
+
+type Log struct {
+	mu     sync.RWMutex
+	Dir    string
+	Config Config
+
+	activeSegment *segment
+	segments      []*segment
+}
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+
+	return l, l.setup()
+}
+
+func (l *Log) setup() error {
+	files, err := os.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+
+	baseOffsets := make([]uint64, 0, len(files))
+	for _, file := range files {
+		offStr := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
+		off, err := strconv.ParseUint(offStr, 10, 0)
+		if err != nil {
+			continue
+		}
+		baseOffsets = append(baseOffsets, off)
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+
+	// NOTE - baseOffsets: index, store 파일을 중복해서 담고 있으므로 2개씩 증가
+	for i := 0; i < len(baseOffsets); i += 2 {
+		if err := l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+	}
+
+	if len(l.segments) == 0 {
+		if err := l.newSegment(l.Config.Segment.InitialOffset); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Log) newSegment(off uint64) error {
+	s, err := newSegment(l.Dir, off, l.Config)
+	if err != nil {
+		return err
+	}
+	l.segments = append(l.segments, s)
+	l.activeSegment = s
+	return nil
+}
+
+func (l *Log) Append(record *pb.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.activeSegment.IsMaxed() {
+		off := l.activeSegment.nextOffset
+		if err := l.newSegment(off); err != nil {
+			return 0, err
+		}
+	}
+
+	return l.activeSegment.Append(record)
+}
+
+func (l *Log) Read(off uint64) (*pb.Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	var s *segment
+	for _, segment := range l.segments {
+		if segment.baseOffset <= off && off < segment.nextOffset {
+			s = segment
+			break
+		}
+	}
+
+	if s == nil || s.nextOffset <= off {
+		return nil, ErrOffsetOutOfRange
+	}
+
+	return s.Read(off)
+}
+
+var ErrOffsetOutOfRange = errors.New("offset out of range")
+
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	for _, segment := range l.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Log) Remove() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+
+	return os.RemoveAll(l.Dir)
+}
+
+func (l *Log) Reset() error {
+	if err := l.Remove(); err != nil {
+		return err
+	}
+
+	return l.setup()
+}
+
+func (l *Log) LowestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	return l.segments[0].baseOffset, nil
+}
+
+func (l *Log) HighestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	off := l.segments[len(l.segments)-1].nextOffset
+	if off == 0 {
+		return 0, nil
+	}
+
+	return off - 1, nil
+}
+
+func (l *Log) Truncate(lowest uint64) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	var segments []*segment
+	for _, s := range l.segments {
+		if s.nextOffset <= lowest+1 {
+			if err := s.Remove(); err != nil {
+				return err
+			}
+			continue
+		}
+		segments = append(segments, s)
+	}
+
+	l.segments = segments
+	return nil
+}
+
+func (l *Log) Reader() io.Reader {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	readers := make([]io.Reader, len(l.segments))
+	for i, segment := range l.segments {
+		readers[i] = &storeReader{segment.store, 0}
+	}
+	return io.MultiReader(readers...)
+}
+
+type storeReader struct {
+	*store
+	off int64
+}
+
+func (s *storeReader) Read(p []byte) (n int, err error) {
+	n, err = s.ReadAt(p, s.off)
+	s.off += int64(n)
+	return
+}
---
+++ pkg/log/log_test.go
@@ -0,0 +1,160 @@
+package log
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/zrma/proglog/pkg/pb"
+)
+
+func TestLog(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		f := newFixture(t)
+
+		want := &pb.Record{
+			Value: []byte("hello world"),
+		}
+
+		off, err := f.log.Append(want)
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), off)
+
+		got, err := f.log.Read(off)
+		require.NoError(t, err)
+		require.Equal(t, want.GetValue(), got.GetValue())
+	})
+
+	t.Run("Err/OffsetOutOfRange", func(t *testing.T) {
+		f := newFixture(t)
+
+		got, err := f.log.Read(1)
+		require.Equal(t, ErrOffsetOutOfRange, err)
+		require.Nil(t, got)
+	})
+
+	t.Run("OK/InitWithExisting", func(t *testing.T) {
+		f := newFixture(t)
+
+		want := &pb.Record{
+			Value: []byte("hello world"),
+		}
+
+		for i := 0; i < 3; i++ {
+			_, err := f.log.Append(want)
+			require.NoError(t, err)
+		}
+		require.NoError(t, f.log.Close())
+
+		lowest, err := f.log.LowestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), lowest)
+
+		highest, err := f.log.HighestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), highest)
+
+		log, err := NewLog(f.log.Dir, f.log.Config)
+		require.NoError(t, err)
+
+		lowest, err = log.LowestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), lowest)
+
+		highest, err = log.HighestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), highest)
+	})
+
+	t.Run("OK/Reader", func(t *testing.T) {
+		f := newFixture(t)
+
+		want := &pb.Record{
+			Value: []byte("hello world"),
+		}
+
+		off, err := f.log.Append(want)
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), off)
+
+		reader := f.log.Reader()
+		b, err := io.ReadAll(reader)
+		require.NoError(t, err)
+
+		rawStr, err := proto.Marshal(want)
+		require.NoError(t, err)
+
+		size := enc.Uint64(b[:lenWidth])
+		require.Equal(t, uint64(len(rawStr)), size)
+		require.Len(t, b[lenWidth:], int(size))
+		require.Equal(t, rawStr, b[lenWidth:])
+
+		got := &pb.Record{}
+		err = proto.Unmarshal(b[lenWidth:], got)
+		require.NoError(t, err)
+		require.Equal(t, want.GetValue(), got.GetValue())
+	})
+
+	t.Run("OK/Truncate", func(t *testing.T) {
+		f := newFixture(t)
+
+		want := &pb.Record{
+			Value: []byte("hello world"),
+		}
+
+		for i := 0; i < 3; i++ {
+			_, err := f.log.Append(want)
+			require.NoError(t, err)
+		}
+
+		_, err := f.log.Read(0)
+		require.NoError(t, err)
+
+		lowest, err := f.log.LowestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(0), lowest)
+
+		highest, err := f.log.HighestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), highest)
+
+		err = f.log.Truncate(1)
+		require.NoError(t, err)
+
+		_, err = f.log.Read(0)
+		require.Error(t, err)
+		require.Equal(t, ErrOffsetOutOfRange, err)
+
+		lowest, err = f.log.LowestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), lowest)
+
+		highest, err = f.log.HighestOffset()
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), highest)
+	})
+}
+
+type fixture struct {
+	log *Log
+}
+
+func newFixture(t *testing.T) *fixture {
+	t.Helper()
+
+	dir, err := os.MkdirTemp(os.TempDir(), "log-test")
+	require.NoError(t, err)
+
+	cfg := Config{}
+	cfg.Segment.MaxStoreBytes = 32
+
+	log, err := NewLog(dir, cfg)
+	require.NoError(t, err)
+
+	return &fixture{
+		log: log,
+	}
+}
