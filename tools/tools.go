7e6fbb20894aa4a7d79b801be8f9bc89243548dd
zrma <bulbitain@gmail.com>
2024-06-25 22:09:45 +0900

    add cert

--- tools/tools.go
+++ tools/tools.go
@@ -4,6 +4,8 @@ package tools
 
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/cloudflare/cfssl/cmd/cfssl"
+	_ "github.com/cloudflare/cfssl/cmd/cfssljson"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
